217885347502cfbb92a85c9555b3a7502e98aca8
Leonardo Negreiros <159206831+LeoNP8@users.noreply.github.com>
2025-09-18 10:59:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let x = "Bom ";
+let y = "dia";
+
+let z = x + y;
+
+alert(z)
+
+let salario = parseFloat(prompt("Digite o seu salario atual: "));
+let novoSalario = salario * 1.25;
+
+alert("O salario atual é R$ " + salario.toFixed(2));
+alert("O seu novo salário será R$ " + novoSalario);
+
